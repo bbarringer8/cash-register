fe2df5b1f401020eb497053387076ba074f6173c
bbarringer8 <bbarring@nebrweslyan.edu>
2014-11-11 09:48:26 -0500

    Added prices and items according to Aaron's code

--- prices.js
+++ prices.js
@@ -1,38 +1,38 @@
 var prices = [
     {
-	name: 'Item',
-	price: 1.99,
+	name: 'Benz',
+	price: 100000,
     },
     {
-	name: 'Item',
-	price: 1.99,
+	name: 'Minivan',
+	price: 30000,
     },
     {
-	name: 'Item',
-	price: 1.99,
+	name: 'Mustang',
+	price: 6000,
     },
     {
-	name: 'Item',
-	price: 1.99,
+	name: 'Mini Coup',
+	price: 45000,
     },
     {
-	name: 'Item',
-	price: 1.99,
+	name: 'Porsche',
+	price: 75000,
     },
     {
-	name: 'Item',
-	price: 1.99,
+	name: 'Ram 1500',
+	price: 50000,
     },
     {
-	name: 'Item',
-	price: 1.99,
+	name: 'Ford F-150',
+	price: 45000,
     },
     {
-	name: 'Item',
-	price: 1.99,
+	name: 'Jaguar',
+	price: 65000,
     },
     {
-	name: 'Item',
-	price: 1.99,
+	name: 'Lambourghini',
+	price: 100000,
     },
 ];
