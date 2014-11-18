1600c38c841c18052314ba15c311ecdf59299a32
aschiller8 <aschiller8@hotmail.com>
2014-11-18 10:31:03 -0500

    added event listeners

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
 "use strict"; // This line chooses a JavaScript dialect, one that helps both jsLint (used in OrionHub) and browsers catch errors.
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 
-function cash_register() {
+function cash_register(item) {
 	var total;
 	
 	
 }
 
+document.getElementById('Benz').addEventListener('click',Benz);
+document.getElementById('Mini_Van').addEventListener('click',Mini_Van);
+document.getElementById('Mustang').addEventListener('click',Mustang);
+document.getElementById('Lambo').addEventListener('click',Lambo);
+document.getElementById('Ram_1500').addEventListener('click',Ram_1500);
+document.getElementById('Ford_F150').addEventListener('click',Ford_F150);
+document.getElementById('Jaguar').addEventListener('click',Jaguar);
+document.getElementById('Porsche').addEventListener('click',Porsche);
+document.createElement('Mini_Coup').addEventListener('click',Mini_Coup);
 
-
-document.createElement(Benz); 	
-document.createElement(Mini_Van);
-document.createElement(Mustang);
-document.createElement(Lambo);
-document.createElement(Ram_1500);
-document.createElement(Ford_F150);
-document.createElement(Jaguar);
-document.createElement(Porsche);
-document.createElement(Mini_Coup)
-
-
-
-
-document.getElementById(benz);
-document.getElementById(Mini_Van);
-document.getElementById(Mustang);
-document.getElementById(Lambo);
-document.getElementById(Ram_1500);
-document.getElementById(Ford_F150);
-document.getElementById(Jaguar);
-document.getElementById(Porsche);
-document.createElement(Mini_Coup);
